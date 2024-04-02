3ba71199c86fe6a48ca3f57edb31ff1f20c8dc83
Kauan Rodrigues Oliveira Rocha <kauan.rocha@yuzer.com.br>
2024-04-02 11:54:15 -0300

    Addig requestMapping

--- src/main/java/com/example/amazonbuckets3/controllers/PublisherController.java
+++ src/main/java/com/example/amazonbuckets3/controllers/PublisherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/files")
 public class PublisherController {
     private final PublisherService publisherService;
 
